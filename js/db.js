f503c7171a50b4ac91be5859e70df0a23b16323e
Marco Almeida <marcoafilho@gmail.com>
2016-07-09 20:21:25 +0200

    Fix the names of some of the Pokémon

--- js/db.js
+++ js/db.js
@@ -1,16 +1,16 @@
 var db = {
     pokemons: [
         'Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie',
-        'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Ratata', 'Raticate',
+        'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate',
         'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'Nidoran Female', 'Nidorina',
         'Nidoqueen', 'Nidoran Male', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff',
         'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat', 'Venomoth', 'Diglett',
         'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe', 'Arcanine', 'Poliwag',
-        'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Macho', 'Machoke', 'Machamp', 'Bellsprout', 'Weepinbell',
+        'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp', 'Bellsprout', 'Weepinbell',
         'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 'Rapidash', 'Slowpoke', 'Slowbro',
         'Magnemite', 'Magneton', 'Farfetch\'d', 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 'Grimer', 'Muk', 'Shellder',
         'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby', 'Kingler', 'Voltorb', 'Electrode',
-        'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Himonchan', 'Lickitung', 'Koffing', 'Weezing',
+        'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan', 'Lickitung', 'Koffing', 'Weezing',
         'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra', 'Goldeen', 'Seaking', 'Staryu',
         'Starmie', 'Mr. Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir', 'Tauros', 'Magikarp', 'Gyarados',
         'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon', 'Omanyte', 'Omastar', 'Kabuto',
@@ -39,7 +39,5 @@ var db = {
         'Sensei', 'Fighter', 'Crazy scientist man', 'Groupie woman', 'Snowboarder', 'Groupie man', 'Team Rocket sergeant',
         'Team Rocket boss', 'Rival', 'Show off', 'Students', 'Dancers', 'Serious man', 'Agent', 'Lucas', 'Dawn', 'Lucas Alternative',
         'Dawn Alternative'
-    ],
-    playerPokemons: [],
-    opponentPokemons: [],
+    ]
 };
